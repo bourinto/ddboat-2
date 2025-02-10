cbd199b30ae13e568ff1c6c9e9c041cfb02a1c3d
Vianney Fleury <vianneyfleury@Vianneys-MacBook-Pro.local>
2025-02-10 14:52:47 +0100

    Implémentation des logs

---
+++ write_log.py
@@ -0,0 +1,19 @@
+import csv
+import time
+
+
+class Log:
+    def __init__(self, headers = ['X', 'Y', 'Cap GPS', 'Cap Compas', 'Correction']):
+        self.headers = headers
+        self.init_log()
+
+    def init_log(self):
+        with open("donnees_gps.csv", mode='w', newline='') as fichier_csv:
+            writer = csv.writer(fichier_csv)
+            writer.writerow(['t']+self.headers)
+
+    def write_log(self, variables):
+        with open("donnees_gps.csv", mode='a', newline='') as fichier_csv:
+            writer = csv.writer(fichier_csv)
+            writer.writerow([time.time()]+variables)
+            print([self.headers[i], " : ", variables[i]] for i in range (len(variables)))
