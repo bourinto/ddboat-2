ecc9e61d3fe16bdfe5f1a6477eb0d285a152098d
Vianney Fleury <vianneyfleury@Vianneys-MacBook-Pro.local>
2025-02-11 11:23:05 +0100

    Added ms_circle.py

---
+++ ms_circle.py
@@ -0,0 +1,33 @@
+from ws3k2_drivers import *
+
+if __name__ == '__main__':
+    # Initialization
+    ws3k2 = WS3K2(['heading', 'correction'])
+
+    # Phase 1: Join the circle
+    coords_buoy =  # TODO : Add the coordinates of the buoy
+    ws3k2.navigate_to_waypoint(coords_buoy, base_speed=150, kp=130, dstop=10)
+
+    # Phase 2 : Follow the circle
+    def circular_traj(t, boat = ws3k2):
+        r = 40
+        T = 200
+
+        boat_nr = 10
+        nb_boats = 18
+
+        # Convert buoy corodinates
+        rho = 6400000
+        lat_buoy = coords_buoy[0]
+        lon_buoy = coords_buoy[1]
+        x_buoy = rho * np.cos(boat.ref_point[0]) * (lon_buoy - boat.ref_point[1])
+        y_buoy = rho * (lat_buoy - boat.ref_point[0])
+        pos_buoy = np.array([x_buoy, y_buoy])
+
+        return pos_buoy + np.array([r*np.cos(2*np.pi*t/T),
+                                       r*np.sin(2*np.pi*t/T)])
+
+    ws3k2.follow_virtual_point(virtual_traj=circular_traj, journey_time=200)
+
+    # Stop motors at the end
+    ws3k2.motor(0, 0)
--- ws3k2_drivers.py
+++ ws3k2_drivers.py
@@ -18,7 +18,7 @@ from write_log import Log
 
 class WS3K2():
     def __init__(self, headers):
-        log = Log(headers)
+        self.logger = Log(headers)
 
         # Initialize IMU, Arduino and GPS
         self.imu = imu_driver.Imu9IO()
@@ -29,21 +29,24 @@ class WS3K2():
         # Load calibration data
         self.bmag, self.Amag = load_calibration("calibration_data.npz")
 
+        # Reference variables
+        self.ref_point = (48.19900500000001, -3.0148363333333332)
+
     def motor(self, spd_left, spd_right):
         self.ard.send_arduino_cmd_motor(spd_left, spd_right)
 
     def heading(self):
         return get_heading(self.imu, self.bmag, self.Amag)
 
-    def loc(self,rho=6400000, ref_point=(48.19900500000001, -3.0148363333333332)):
+    def loc(self,rho=6400000):
         # Read GPS
         gps = get_gps(self.gps_device)
         lat_gps = np.radians(gps[0])
         lon_gps = np.radians(gps[1])
 
         # Convert GPS reading
-        x_gps = rho * np.cos(ref_point[0]) * (lon_gps - ref_point[1])
-        y_gps = rho * (lat_gps - ref_point[0])
+        x_gps = rho * np.cos(self.ref_point[0]) * (lon_gps - self.ref_point[1])
+        y_gps = rho * (lat_gps - self.ref_point[0])
         pos = np.array([x_gps, y_gps])
         return pos
 
@@ -56,16 +59,16 @@ class WS3K2():
             correction = kp * sawtooth(error)
             self.motor(base_speed + correction, base_speed - correction)
 
-            self.write_log([np.degrees(heading), correction])
+            self.logger.write_log([np.degrees(heading), correction])
 
             time.sleep(1. / Hz)
 
-    def navigate_to_waypoint(self, ref_point, aimed_point, base_speed, kp, dstop=5, Hz=10):
+    def navigate_to_waypoint(self, aimed_point=np.array([48.199665, -3.0154350000000014]), base_speed=150 , kp=130, dstop=5, Hz=10):
         rho = 6400000  # approximate Earth radius in meters
 
         # Convert points (lat, lon in degrees) to radians
-        lat_ref = np.radians(ref_point[0])
-        lon_ref = np.radians(ref_point[1])
+        lat_ref = np.radians(self.ref_point[0])
+        lon_ref = np.radians(self.ref_point[1])
 
         lat_aimed = np.radians(aimed_point[0])
         lon_aimed = np.radians(aimed_point[1])
@@ -93,3 +96,33 @@ class WS3K2():
             self.logger.write_log([pos[0], pos[1], target_heading, heading, correction, distance])
 
             time.sleep(1. / Hz)
+
+
+    def follow_virtual_point(self, virtual_traj, journey_time):
+        t0 = time.time()
+        t = 0
+
+        heading_kP = 130
+        speed_kP = 15
+
+        while t < journey_time:
+            t = time.time()-t0
+            target = virtual_traj(t)
+
+            error = target - self.loc
+            target_heading = np.arctan2(error[1], error[0])
+            heading = self.heading()
+            heading_error = sawtooth(target_heading - heading)
+            heading_correction = heading_kP * heading_error
+
+            distance_error = np.linalg.norm(error)
+            if distance_error < 10:
+                base_speed = speed_kP * distance_error
+            else:
+                base_speed = 150
+
+            self.motor(base_speed + heading_correction, base_speed - heading_correction)
+
+            self.logger.write_log([np.degrees(heading), heading_correction])
+
+            time.sleep(0.1)
